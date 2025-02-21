b656666263e527aa71b680f77329c0033c1dcbeb
Felipe Calazans do Prado Carvalho <110859636+calazanha@users.noreply.github.com>
2025-02-21 20:53:39 -0300

    Create Lista2-Q1.py

---
+++ Lista2/Lista2-Q1.py
@@ -0,0 +1,8 @@
+"""
+1 - Escreva um programa que solicite um número inteiro ao usuário e informe se ele é par ou
+"""
+num = int(input("Insira um número: "))
+if num % 2 == 0:
+    print("Par")
+else:
+    print("Impar")
