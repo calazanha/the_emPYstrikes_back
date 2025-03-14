cefc1851e169578a6569b7c4d53ec156d8e93952
Felipe Calazans do Prado Carvalho <110859636+calazanha@users.noreply.github.com>
2025-03-14 19:14:04 -0300

    Update exercicio3.py

--- Lista4/exercicio3.py
+++ Lista4/exercicio3.py
@@ -1,1 +1,6 @@
-
+numeros = list(map(int, input("Digite uma lista de números inteiros separados por espaço: ").split()))
+sem_duplicatas = []
+for numero in numeros:
+    if numero not in sem_duplicatas:
+        sem_duplicatas.append(numero)
+print("Lista sem duplicatas:", sem_duplicatas)
