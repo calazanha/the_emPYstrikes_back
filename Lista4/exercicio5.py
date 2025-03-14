d745eccf0fad342145dc82d04f4e898485875aa3
Felipe Calazans do Prado Carvalho <110859636+calazanha@users.noreply.github.com>
2025-03-14 19:14:55 -0300

    Update exercicio5.py

--- Lista4/exercicio5.py
+++ Lista4/exercicio5.py
@@ -1,1 +1,4 @@
-
+numeros = list(map(int, input("Digite uma lista de números inteiros separados por espaço: ").split()))
+numero_especifico = int(input("Digite o número que deseja contar na lista: "))
+ocorrencias = numeros.count(numero_especifico)
+print(f"O número {numero_especifico} aparece {ocorrencias} vezes na lista.")
