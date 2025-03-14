2228b6b62e9c59015af97eece6dce596e9c22bde
Felipe Calazans do Prado Carvalho <110859636+calazanha@users.noreply.github.com>
2025-03-14 19:18:53 -0300

    Create exercicio5.py

---
+++ Lista5/exercicio5.py
@@ -0,0 +1,5 @@
+def celsius_para_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+celsius = float(input("Digite a temperatura em Celsius: "))
+fahrenheit = celsius_para_fahrenheit(celsius)
+print(f"{celsius}°C equivale a {fahrenheit}°F.")
