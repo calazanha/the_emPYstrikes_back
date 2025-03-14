efa4285a42472fed78dc3ff887cec40c930a2564
Felipe Calazans do Prado Carvalho <110859636+calazanha@users.noreply.github.com>
2025-03-14 19:13:35 -0300

    Update exercicio2.py

--- Lista4/exercicio2.py
+++ Lista4/exercicio2.py
@@ -1,1 +1,5 @@
-
+numeros = list(map(int, input("Digite uma lista de números inteiros separados por espaço: ").split()))
+maior = max(numeros)
+menor = min(numeros)
+print("O maior número é:", maior)
+print("O menor número é:", menor)
