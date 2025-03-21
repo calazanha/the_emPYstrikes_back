a401bcc35af649ace9411534b276659859207d9e
Felipe Calazans do Prado Carvalho <110859636+calazanha@users.noreply.github.com>
2025-03-21 19:26:36 -0300

    Create ex1.py

---
+++ Lista6/ex1.py
@@ -0,0 +1,6 @@
+produto = ("Notebook", 3500.00, 10)
+
+print("=== Informações do Produto ===")
+print(f"Nome: {produto[0]}")
+print(f"Preço: R${produto[1]:.2f}")
+print(f"Quantidade em Estoque: {produto[2]} unidades")
