c3ae7cdf2617e040d20ce7ed83ad8281591e1155
Felipe Calazans do Prado Carvalho <110859636+calazanha@users.noreply.github.com>
2025-03-28 18:53:20 -0300

    Create ex1.py

---
+++ Lista8/ex1.py
@@ -0,0 +1,12 @@
+def divisao_segura():
+    try:
+        num1 = float(input("Digite o primeiro número: "))
+        num2 = float(input("Digite o segundo número: "))
+        resultado = num1 / num2
+        print(f"O resultado da divisão é: {resultado}")
+    except ZeroDivisionError:
+        print("Erro: Não é possível dividir por zero.")
+    except ValueError:
+        print("Erro: Entrada inválida. Digite apenas números.")
+
+divisao_segura()
